e356cbacc6475a610113193b9aa4c8ad06ca75d0
Романский Алексей <kirbi.zsthan@gmail.com>
2025-11-26 13:45:19 +0300

    Ещё приготовления

--- CS332-Lab11/shaders.h
+++ CS332-Lab11/shaders.h
@@ -13,18 +13,25 @@ extern Task currentAssignment;
 
 // это нужно для хранения программ шейдеров
 extern GLuint ProgramQuad, ProgramFan, ProgramPentagon;
+extern GLuint ProgramQuadUniform, ProgramFanUniform, ProgramPentagonUniform;
+extern GLuint ProgramQuadGradient, ProgramFanGradient, ProgramPentagonGradient;
 
 // это индекс атрибута для вершин
-extern GLint Attrib_vertex;
+extern GLint Attrib_vertex, Attrib_color;
 
 // эта штука нужна для загрузки вершин в видеобуфер
 extern GLuint VBOQuad, VBOFan, VBOPentagon;
+extern GLuint VBO_Gradient;
 
 extern const char* VertexShaderSource;
 
 extern const char* FragShaderQuadSource;
 extern const char* FragShaderFanSource;
 extern const char* FragShaderPentagonSource;
+
+extern const char* FragShaderUniformSource;
+
+extern const char* FragShaderQuadSource;
 
 // структура для хранения координат вершин
 struct Vertex {
