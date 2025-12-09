eb5708c169a33e62a9800e60107481c21bde721a
Voronin Egor <evoroni2004@gmail.com>
2025-12-10 02:28:21 +0300

    Revert "chore: fan to triangles"
    This reverts commit 7d292e37b34c78c243cabfe98234bc2ac4351472.

--- CS332-Lab11/shaders.cpp
+++ CS332-Lab11/shaders.cpp
@@ -163,11 +163,7 @@ void InitVBO() {
     glGenBuffers(1, &VBO_GradientPentagon);
 
     // Quad
-    Vertex quad[6] = {
-        {-0.95f,-0.3f}, {-0.95f,0.3f}, {-0.35f,0.3f}, // первый треугольник
-        {-0.95f,-0.3f}, {-0.35f,0.3f}, {-0.35f,-0.3f}  // второй треугольник
-    };
-
+    Vertex quad[4] = { {-0.95f,-0.3f},{-0.95f,0.3f},{-0.35f,0.3f},{-0.35f,-0.3f} };
     glBindBuffer(GL_ARRAY_BUFFER, VBOQuad);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
 
@@ -179,85 +175,41 @@ void InitVBO() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(quadGrad), quadGrad, GL_STATIC_DRAW);
 
     // Fan
-    //ColorVertex fanGrad[6];
-    //float cx = 0.0f, cy = 0.0f, r = 0.3f;
-    //fanGrad[0] = { cx,cy,1,1,1,1 };
-    //for (int i = 1; i <= 5; i++) {
-    //    float angle = M_PI * (i - 1) / 4.0f;
-    //    fanGrad[i] = { cx + r * cos(angle),cy + r * sin(angle),float(i) / 5.0f,1.0f - float(i) / 5.0f,0.5f,1 };
-    //}
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO_GradientFan);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(fanGrad), fanGrad, GL_STATIC_DRAW);
-
-    //Vertex fan[6]; fan[0] = { cx,cy };
-    //for (int i = 1; i <= 5; i++) {
-    //    float angle = M_PI * (i - 1) / 4.0f;
-    //    fan[i] = { cx + r * cos(angle),cy + r * sin(angle) };
-    //}
-    //glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(fan), fan, GL_STATIC_DRAW);
-
-    //Vertex fan[12];
-    //float cx = 0.0f, cy = 0.0f, r = 0.3f;
-    //Vertex center = { cx, cy };
-    //for (int i = 0; i < 4; i++) {
-    //    float angle1 = M_PI * i / 4.0f;
-    //    float angle2 = M_PI * (i + 1) / 4.0f;
-    //    fan[i * 3] = center;
-    //    fan[i * 3 + 1] = { cx + r * cos(angle1), cy + r * sin(angle1) };
-    //    fan[i * 3 + 2] = { cx + r * cos(angle2), cy + r * sin(angle2) };
-    //}
-    //glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(fan), fan, GL_STATIC_DRAW);
-
-    {
-        Vertex fan[12];
-        float cx = 0.0f, cy = 0.0f, r = 0.3f;
-        Vertex center = { cx, cy };
-        for (int i = 0; i < 4; i++) {
-            float angle1 = M_PI * i / 4.0f;
-            float angle2 = M_PI * (i + 1) / 4.0f;
-            fan[i * 3] = center;
-            fan[i * 3 + 1] = { cx + r * cos(angle1), cy + r * sin(angle1) };
-            fan[i * 3 + 2] = { cx + r * cos(angle2), cy + r * sin(angle2) };
-        }
-        glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(fan), fan, GL_STATIC_DRAW);
-
+    ColorVertex fanGrad[6];
+    float cx = 0.0f, cy = 0.0f, r = 0.3f;
+    fanGrad[0] = { cx,cy,1,1,1,1 };
+    for (int i = 1; i <= 5; i++) {
+        float angle = M_PI * (i - 1) / 4.0f;
+        fanGrad[i] = { cx + r * cos(angle),cy + r * sin(angle),float(i) / 5.0f,1.0f - float(i) / 5.0f,0.5f,1 };
     }
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_GradientFan);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(fanGrad), fanGrad, GL_STATIC_DRAW);
+
+    Vertex fan[6]; fan[0] = { cx,cy };
+    for (int i = 1; i <= 5; i++) {
+        float angle = M_PI * (i - 1) / 4.0f;
+        fan[i] = { cx + r * cos(angle),cy + r * sin(angle) };
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(fan), fan, GL_STATIC_DRAW);
 
     // Pentagon
-    //ColorVertex pentGrad[5];
-    //float px = 0.65f, py = 0.0f, pr = 0.3f;
-    //for (int i = 0; i < 5; i++) {
-    //    float angle = 2 * M_PI * i / 5.0 + M_PI / 2;
-    //    pentGrad[i] = { px + pr * cos(angle),py + pr * sin(angle),float(i) / 5.0f,0.5f,float(i) / 5.0f,1 };
-    //}
-    //glBindBuffer(GL_ARRAY_BUFFER, VBO_GradientPentagon);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(pentGrad), pentGrad, GL_STATIC_DRAW);
-
-    //Vertex pent[5];
-    //for (int i = 0; i < 5; i++) {
-    //    float angle = 2 * M_PI * i / 5.0 + M_PI / 2;
-    //    pent[i] = { px + pr * cos(angle),py + pr * sin(angle) };
-    //}
-    //glBindBuffer(GL_ARRAY_BUFFER, VBOPentagon);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(pent), pent, GL_STATIC_DRAW);
-
-    {
-        Vertex pent[9];
-        float px = 0.65f, py = 0.0f, pr = 0.3f;
-        Vertex pentCenter = { px, py };
-        for (int i = 0; i < 3; i++) {
-            float angle1 = 2 * M_PI * i / 5.0 + M_PI / 2;
-            float angle2 = 2 * M_PI * (i + 1) / 5.0 + M_PI / 2;
-            pent[i * 3] = pentCenter;
-            pent[i * 3 + 1] = { px + pr * cos(angle1), py + pr * sin(angle1) };
-            pent[i * 3 + 2] = { px + pr * cos(angle2), py + pr * sin(angle2) };
-        }
-        glBindBuffer(GL_ARRAY_BUFFER, VBOPentagon);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(pent), pent, GL_STATIC_DRAW);
+    ColorVertex pentGrad[5];
+    float px = 0.65f, py = 0.0f, pr = 0.3f;
+    for (int i = 0; i < 5; i++) {
+        float angle = 2 * M_PI * i / 5.0 + M_PI / 2;
+        pentGrad[i] = { px + pr * cos(angle),py + pr * sin(angle),float(i) / 5.0f,0.5f,float(i) / 5.0f,1 };
     }
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_GradientPentagon);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pentGrad), pentGrad, GL_STATIC_DRAW);
+
+    Vertex pent[5];
+    for (int i = 0; i < 5; i++) {
+        float angle = 2 * M_PI * i / 5.0 + M_PI / 2;
+        pent[i] = { px + pr * cos(angle),py + pr * sin(angle) };
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, VBOPentagon);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pent), pent, GL_STATIC_DRAW);
 }
 
 void Init() { 
@@ -267,61 +219,54 @@ void Init() {
 
 void Draw() {
     if (currentAssignment == Task2) {
-        // Quad (два треугольника)
-        glUseProgram(ProgramQuad);
-        glEnableVertexAttribArray(Attrib_vertex);
+        glUseProgram(ProgramQuad); glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOQuad);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 6); // 6 вершин для двух треугольников
-        glDisableVertexAttribArray(Attrib_vertex);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4); glDisableVertexAttribArray(Attrib_vertex);
 
-        // Fan (4 треугольника, 12 вершин)
-        glUseProgram(ProgramFan);
-        glEnableVertexAttribArray(Attrib_vertex);
+        glUseProgram(ProgramFan); glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 12); // 12 вершин для 4 треугольников
-        glDisableVertexAttribArray(Attrib_vertex);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6); glDisableVertexAttribArray(Attrib_vertex);
 
-        // Pentagon (3 треугольника, 9 вершин)
-        glUseProgram(ProgramPentagon);
-        glEnableVertexAttribArray(Attrib_vertex);
+        glUseProgram(ProgramPentagon); glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOPentagon);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 9); // 9 вершин для 3 треугольников
-        glDisableVertexAttribArray(Attrib_vertex);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 5); glDisableVertexAttribArray(Attrib_vertex);
     }
     else if (currentAssignment == Task3) {
-        // четырёхугольник (два треугольника)
+        // четырёхугольник 
         glUseProgram(ProgramQuadUniform);
         GLint colorLocQuad = glGetUniformLocation(ProgramQuadUniform, "uColor");
         glUniform4f(colorLocQuad, 0.0f, 1.0f, 0.0f, 1.0f);
         glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOQuad);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         glDisableVertexAttribArray(Attrib_vertex);
         glUseProgram(0);
 
-        // веер (4 треугольника)
+        // веер 
         glUseProgram(ProgramFanUniform);
         GLint colorLocFan = glGetUniformLocation(ProgramFanUniform, "uColor");
         glUniform4f(colorLocFan, 0.0f, 0.0f, 1.0f, 1.0f);
         glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOFan);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 12);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
         glDisableVertexAttribArray(Attrib_vertex);
         glUseProgram(0);
-
-        // пятиугольник (3 треугольника)
+        // пятиугольник 
         glUseProgram(ProgramPentagonUniform);
         GLint colorLocPentagon = glGetUniformLocation(ProgramPentagonUniform, "uColor");
         glUniform4f(colorLocPentagon, 1.0f, 0.0f, 0.0f, 1.0f);
         glEnableVertexAttribArray(Attrib_vertex);
         glBindBuffer(GL_ARRAY_BUFFER, VBOPentagon);
         glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 9);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 5);
         glDisableVertexAttribArray(Attrib_vertex);
         glUseProgram(0);
     }
